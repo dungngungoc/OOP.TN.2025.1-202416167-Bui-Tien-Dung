665f7cb57d59bcc8098217bfb53705403374f4fc
Bui Tien Dung <tiendungbui2006@gmail.com>
2025-11-17 08:09:58 +0700

    Feat: Create Store class and StoreTest

--- AimsProject/src/com/hust/kstn/models/Store.java
+++ AimsProject/src/com/hust/kstn/models/Store.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hust.kstn.models;
 
-/**
- *
- * @author Admin
- */
 public class Store {
+    public static final int MAX_ITEMS_IN_STORE = 100;
     
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAX_ITEMS_IN_STORE];
+    
+    private int qtyItems = 0;
+
+    public void addDVD(DigitalVideoDisc disc) {
+        if (qtyItems >= MAX_ITEMS_IN_STORE) {
+            System.out.println("The store is full.");
+        } else {
+            itemsInStore[qtyItems] = disc;
+            qtyItems++;
+            System.out.println("The disc has been added to the store.");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        if (qtyItems == 0) {
+            System.out.println("The store is empty.");
+            return;
+        }
+
+        int indexToRemove = -1;
+        for (int i = 0; i < qtyItems; i++) {
+            if (itemsInStore[i] == disc) { 
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (indexToRemove == -1) {
+            System.out.println("The disc does not exist in the store.");
+        } else {
+            for (int i = indexToRemove; i < qtyItems - 1; i++) {
+                itemsInStore[i] = itemsInStore[i + 1];
+            }
+            
+            itemsInStore[qtyItems - 1] = null;
+            qtyItems--;
+            System.out.println("The disc has been removed from the store.");
+        }
+    }
 }

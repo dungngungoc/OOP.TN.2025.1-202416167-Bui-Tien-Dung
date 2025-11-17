8be622d65aaf4860d7abd2af3aa59fa84688d9f5
Bui Tien Dung <tiendungbui2006@gmail.com>
2025-11-17 10:08:01 +0700

    Refactor: Implement Media and Disc inheritance hierarchy (2.8)

--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -1,77 +1,37 @@
 package com.hust.kstn.models;
 
-public class DigitalVideoDisc {
-    private static int nbDigitalVideoDiscs = 0;
-    private int id;
-
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private double cost;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public double getCost() {
-        return cost;
-    }
+public class DigitalVideoDisc extends Disc {
     
     public DigitalVideoDisc(String title) {
-        this.title = title;
-        
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+        super(title, 0.0, "", 0, "");
     }
 
     public DigitalVideoDisc(String category, String title, double cost) {
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+        super(title, cost, category, 0, "");
     }
 
     public DigitalVideoDisc(String director, String category, String title, double cost) {
-        this.director = director;
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+        super(title, cost, category, 0, director);
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-        
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+        super(title, cost, category, length, director);
     }
-
-    // MỤC 2.3: GHI ĐÈ PHƯƠNG THỨC toString()
+    
     @Override
     public String toString() {
-        // Trả về một chuỗi theo định dạng: DVD - [ID] - [Title] - [Category] - [Director] - [Length]: [Cost] $
-        return "DVD - [" + id + "] - [" + title + "] - [" + category + "] - [" 
-                + director + "] - [" + length + "]: [" + cost + "] $";
+        return "DigitalVideoDisc " + super.toString() 
+                + "[" + this.getLength() + "][" + this.getDirector() + "]";
     }
+
+    String getTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getCost() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+  
+    
 }
